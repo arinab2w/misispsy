9818d48eb5afa4855ed2537de9c1ed5920d7d7df
arinab2w <arinagladkov@gmail.com>
2024-10-21 12:57:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,147 +1,238 @@
-import telebot
-import os
-import csv
-import random
-import const
-import threading
+# Copyright (c) 2023 Minniti Julien
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of Anonymous Telebot and associated documentation files, to deal
+# in Anonymous Telebot without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of Anonymous Telebot, and to permit persons to whom Anonymous Telebot is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of Anonymous Telebot.
+
+# Anonymous Telebot IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH Anonymous Telebot OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from datetime import datetime
 import time
+import telepot
+from telepot.loop import MessageLoop
+import os
+import json
+import requests
 
-bot = telebot.TeleBot(const.TOKEN)
-bot.set_webhook()
 
-# Хранение времени последней активности пользователя
-user_activity = {}
+# Retrieve information of owner, whitelist and bot
+def secret():
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    secrets_path = os.path.join(script_directory, 'SECRETS.json')
+    with open(secrets_path, 'r') as secrets_file:
+        secrets = json.load(secrets_file)
 
-# Максимальное время неактивности в секундах (10 минут = 600 секунд)
-INACTIVITY_TIMEOUT = 600
+    chat_id_key = secrets['id']
+    chat_id_owner = secrets['id_owner']
+    password = secrets['password']
+    token = secrets['token']
 
-# Флаг для остановки бота
-stop_bot = False
+    secrets_file.close()
 
-# Добавляем пользователя в базу
-def add_companion_to_database(tg_id):
-    csv_file = 'data/users.csv'
-    exists = False
-    with open(csv_file, 'r') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            if not row:
-                break
-            if row[0] == str(tg_id):
-                exists = True
-                break
-    if not exists:
-        with open(csv_file, 'a', newline='') as f:
-            writer = csv.writer(f)
-            writer.writerow([tg_id, '+'])
+    return chat_id_key, chat_id_owner, password, token
 
-# Поиск свободного собеседника
-def find_companion_from_database(usr):
-    csv_file = 'data/users.csv'
-    values = []
-    with open(csv_file, 'r', newline='') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            value1, value2 = row
-            if value2 == "+":
-                values.append(value1)
-    if usr in values:
-        values.remove(usr)
-    if not values:
-        return "Empty"
-    return random.choice(values)
 
-# Установка собеседника
-def set_companion(usr1, usr2):
-    connections_file = 'data/users.csv'
-    with open(connections_file, 'r', newline='') as f:
-        reader = csv.reader(f)
-        rows = list(reader)
-        for i, row in enumerate(rows):
-            if row[0] == usr1:
-                rows[i][1] = usr2
-                break
-    with open(connections_file, 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerows(rows)
+# Change password
+def change_password(new_password):
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    secrets_path = os.path.join(script_directory, 'SECRETS.json')
+    with open(secrets_path, 'r') as secrets_file:
+        secrets = json.load(secrets_file)
+    secrets['password'] = new_password
+    with open(secrets_path, 'w') as secrets_file:
+        json.dump(secrets, secrets_file)
 
-# Получение текущего собеседника
-def get_companion(usr):
-    connections_file = 'data/users.csv'
-    with open(connections_file, 'r', newline='') as f:
-        reader = csv.reader(f)
-        rows = list(reader)
-        for i, row in enumerate(rows):
-            if row[0] == usr:
-                return rows[i][1]
 
-# Функция для завершения общения по истечении времени
-def check_inactivity():
-    while not stop_bot:
-        current_time = time.time()
-        for user_id, last_active in list(user_activity.items()):
-            if current_time - last_active > INACTIVITY_TIMEOUT:
-                bot.send_message(user_id, "Вы были отключены за неактивность.")
-                set_companion(user_id, '+')
-                del user_activity[user_id]
-        time.sleep(10)
+# Initializing of Question To Answer
+def qta_set(chat_id, answer):
+    qta[chat_id] = {'answer': answer}
 
-# Обработка текстовых сообщений
-@bot.message_handler(content_types=['text'])
-def handle_text(message):
-    global stop_bot
-    if message.text == '/start':
-        handle_start(message)
-    elif message.text == '/change':
-        handle_change(message)
-    elif message.text == '/stop':
-        bot.send_message(message.chat.id, "Бот отключается. Спасибо за использование!")
-        stop_bot = True
-        bot.stop_polling()  # Остановка поллинга
-    else:
-        update_user_activity(message.chat.id)
-        if get_companion(str(message.chat.id)) != '+':
-            msg = message.text
-            companion = get_companion(str(message.chat.id))
-            bot.send_message(int(companion), msg)
+
+# Help
+def help(chat_id):
+    help_text = " /help - For help obviously\n/users - List of users\n/delete_account - To delete your account here\n/REPORT - To report someone. /USEROWNER will contact you.\n\n" \
+                "How to send message to someone (e.g. USER1):\n/USER1 Hello World\n\n" \
+                "You can also send pictures, photos and videos by adding recipient to caption like for message\n\n" \
+                "If you need more help send message to /USEROWNER\n\n"
+    bot.sendMessage(chat_id, help_text, parse_mode='HTML')
+
+
+# Display user list
+def users(chat_id):
+    users_text = "/USEROWNER"
+    for i in inverse_chat_id_verified:
+        id_user = inverse_chat_id_verified[i]
+        users_text += f"\n{id_user}"
+    bot.sendMessage(chat_id, users_text, parse_mode='HTML')
+
+
+# Main
+def handle(msg):
+    content_type, _, chat_id = telepot.glance(msg)  # Retrieve information about message
+
+    # Is a Question to Answer ?
+    try:
+        answer = qta[chat_id]['answer']
+    except KeyError:
+        answer = '0'
+
+    # Type of message
+    if content_type == "text":  # TEXT
+        command = msg['text']
+    elif content_type == "photo":  # PHOTOS
+        if 'caption' in msg:
+            command = msg['caption']
+            photos = msg["photo"]
+            photo = photos[-1]
+            photo_id = photo['file_id']
         else:
-            bot.send_message(message.chat.id, "На данный момент Вы не состоите в диалоге")
+            command = ''
+    elif content_type == 'video':  # VIDEO
+        if 'caption' in msg:
+            command = msg['caption']
+            videos = msg["video"]
+            video_id = videos['file_id']
+        else:
+            command = ""
 
-# Функция обновления активности пользователя
-def update_user_activity(user_id):
-    user_activity[user_id] = time.time()
+    # Verify if it's a known user or owner
+    if chat_id in chat_id_verified.values() or chat_id == chat_id_owner:
 
-# Обработка команды /start
-def handle_start(message):
-    add_companion_to_database(message.from_user.id)
-    name = message.from_user.first_name
-    keyboard = telebot.types.InlineKeyboardMarkup()
-    keyboard.row(
-        telebot.types.InlineKeyboardButton("Найти собеседника", callback_data="find_companion")
-    )
-    bot.send_message(message.chat.id, f"Привет, {name}!\n\nНажмите кнопку \"Найти собеседника\" для того, чтобы начать общение!", reply_markup=keyboard)
-    update_user_activity(message.from_user.id)
+        # Utils
+        if command == "/help":  # Send /help
+            help(chat_id)
 
-# Обработка команды /change
-def handle_change(message):
-    old = get_companion(str(message.chat.id))
-    if old != '+':
-        bot.send_message(int(old), "С Вами больше не общаются. Вы можете найти нового собеседника, введя команду /change")
-    companion = find_companion_from_database(str(message.chat.id))
-    set_companion(old, '+')
-    if companion == "Empty":
-        bot.send_message(message.chat.id, "К сожалению, свободных собеседников нет")
-        set_companion(str(message.chat.id), '+')
-    else:
-        set_companion(str(message.chat.id), companion)
-        set_companion(companion, str(message.chat.id))
-        bot.send_message(message.chat.id, "Собеседник найден!\n\nНапишите сообщение.")
-        bot.send_message(companion, "Собеседник найден!\n\nНапишите сообщение.")
-    update_user_activity(message.chat.id)
+        elif command == "/users":  # Send users list
+            users(chat_id)
 
-# Запуск функции проверки неактивности пользователей в отдельном потоке
-inactivity_thread = threading.Thread(target=check_inactivity)
-inactivity_thread.start()
+        elif command == '/delete_account' and chat_id != chat_id_owner:  # Delete your user
+            bot.sendMessage(chat_id, f"See you soon {inverse_chat_id_verified[chat_id]}")
+            del chat_id_verified[inverse_chat_id_verified[chat_id]]
 
-# Запуск бота
-bot.infinity_polling()
+        elif command == '/REPORT':  # Report a user  (question)
+            bot.sendMessage(chat_id, f'Who wanted to report you?')
+            answer = 1
+            qta_set(chat_id, answer)
+
+        elif answer == 1:  # Report a user (answer)
+            del qta[chat_id]
+            bot.sendMessage(chat_id,
+                            f"/USEROWNER is alerted about your report about {command}. You will be contacted soon by /USEROWNER")
+            bot.sendMessage(chat_id_owner, f"REPORT of {command} by {inverse_chat_id_verified[chat_id]}")
+
+        # Message from VERIFIED
+        elif command.startswith("/USER") and chat_id != chat_id_owner:
+
+            if command.startswith("/USEROWNER"):  # Send messages to OWNER
+                if content_type == 'photo':
+                    bot.sendPhoto(chat_id_owner, photo_id, f"From {inverse_chat_id_verified[chat_id]} to {command}")
+                elif content_type == 'video':
+                    bot.sendVideo(chat_id_owner, video_id, caption=f"From {inverse_chat_id_verified[chat_id]} to {command}")
+                else:
+                    bot.sendMessage(chat_id_owner, f"From {inverse_chat_id_verified[chat_id]} to {command}")
+
+            else:
+                for id_verified in chat_id_verified:  # Send messages to USER
+                    if command.startswith(id_verified):
+                        if content_type == 'photo':
+                            bot.sendPhoto(chat_id_verified[id_verified], photo_id,
+                                          f"From {inverse_chat_id_verified[chat_id]} to {command}")
+                        elif content_type == 'video':
+                            bot.sendVideo(chat_id_verified[id_verified], video_id,
+                                          caption=f"From {inverse_chat_id_verified[chat_id]} to {command}")
+                        else:
+                            bot.sendMessage(chat_id_verified[id_verified],
+                                            f"From {inverse_chat_id_verified[chat_id]} to {command}")
+
+        # Message from OWNER
+        elif chat_id == chat_id_owner:
+            if command == "/waiting":  # Send list of waiting ID
+                bot.sendMessage(chat_id_owner, chat_id_waiting)
+
+            elif command == "/verified":  # Send list of verified ID
+                bot.sendMessage(chat_id_owner, chat_id_verified)
+
+            elif command == "/banned":  # Send list of banned ID
+                bot.sendMessage(chat_id_owner, chat_id_banned)
+
+            elif command.startswith("/USER"):  # Send messages to USER by OWNER
+                for id_verified in chat_id_verified:
+                    if command.startswith(id_verified):
+                        if content_type == 'photo':
+                            bot.sendPhoto(chat_id_verified[id_verified], photo_id,
+                                          f"From /USEROWNER to {command}")
+                        elif content_type == 'video':
+                            bot.sendVideo(chat_id_verified[id_verified], video_id,
+                                          caption=f"From /USEROWNER to {command}")
+                        else:
+                            bot.sendMessage(chat_id_verified[id_verified],
+                                            f"From /USEROWNER to {command}")
+
+            elif command.startswith("/setpassword "):  # Change password
+                new_password = command.split("/setpassword ")[1]
+                change_password(new_password)
+                _, _, password, _ = secret()
+                bot.sendMessage(chat_id_owner, f"Password changed to {password}")
+
+            elif command == "/password":  # Send password to OWNER
+                _, _, password, _ = secret()
+                bot.sendMessage(chat_id_owner, f"Password: {password}")
+
+            elif command.startswith("/DEL "):  # Ban a user
+                user = command.split("/DEL ")[1]
+                if user in inverse_chat_id_verified.values():
+                    bot.sendMessage(chat_id_verified[user], "Access removed. You are banned.")
+                    chat_id_banned.append(chat_id_verified[user])
+                    del chat_id_verified[user]
+                    bot.sendMessage(chat_id, f"{user} banned.")
+
+    # Connection to the Anonymous Bot
+    elif chat_id not in chat_id_key:
+        if chat_id not in chat_id_waiting and chat_id not in chat_id_verified.values() and chat_id not in chat_id_banned:  # For new users
+            chat_id_waiting.append(chat_id)
+            message_to_unknown_id = f'Welcome to Anonymous TelegramBot, please enter password.'
+            bot.sendMessage(chat_id, message_to_unknown_id)
+
+        elif chat_id in chat_id_banned:  # Block banned users
+            bot.sendMessage(chat_id, f"Access removed. You are banned.")
+
+        else:
+            _, _, password, _ = secret()  # Valid or not users access
+            if command == password:
+                chat_id_verified["/USER" + str(len(chat_id_verified) + 1)] = chat_id
+                inverse_chat_id_verified.update({v: k for k, v in chat_id_verified.items()})
+                chat_id_waiting.remove(chat_id)
+                message_to_unknown_id = f'Connection done, congratulation. You are {inverse_chat_id_verified[chat_id]}. See /help to know how to use the ChatBot'
+                bot.sendMessage(chat_id, message_to_unknown_id)
+            else:
+                message_to_unknown_id = f'Wrong password.'
+                bot.sendMessage(chat_id, message_to_unknown_id)
+
+
+chat_id_waiting = []
+chat_id_verified = {}
+chat_id_banned = []
+inverse_chat_id_verified = {}
+qta = {}
+
+chat_id_key, chat_id_owner, _, token = secret()
+bot = telepot.Bot(token)
+MessageLoop(bot, handle).run_as_thread()
+print('Im listening...')
+for id_user in chat_id_key:
+    bot.sendMessage(id_user, 'Anonymous ChatBot open')
+
+while True:
+    time.sleep(1)
